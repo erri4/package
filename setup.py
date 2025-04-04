4786c0a32a4ae33eba3976c6203e7dfe0b7c5087
rif av <reef.abramson@gmail.com>
2025-04-04 08:57:37 +0300

    .

--- setup.py
+++ setup.py
@@ -57,10 +57,10 @@ if i == 'WebsocketServer':
     current_directory = os.getcwd()
     pip_bat_path = os.path.join(current_directory, 'pip.bat')
     subprocess.run([pip_bat_path, 'WebsocketServer', version, preversion])
-    with open('C:/users/reef/documents/websocket/README.md', 'r') as readme:
-        new = readme.read().replace(f'https://erri4.github.io/package/filesforpip/WebsocketServer-{preversion}-py3-none-any.whl', f'https://erri4.github.io/package/filesforpip/WebsocketServer-{version}-py3-none-any.whl')
-    with open('C:/users/reef/documents/websocket/README.md', 'w') as readme:
-        readme.write(new)
+    with open('C:/users/reef/documents/websocket/requirements.txt', 'r') as requirements:
+        new = requirements.read().replace(f'https://erri4.github.io/package/filesforpip/WebsocketServer-{preversion}-py3-none-any.whl', f'https://erri4.github.io/package/filesforpip/WebsocketServer-{version}-py3-none-any.whl')
+    with open('C:/users/reef/documents/websocket/requirements.txt', 'w') as requirements:
+        requirements.write(new)
     with open('README.md', 'r') as readme:
         new = readme.read().replace(f'https://erri4.github.io/package/filesforpip/WebsocketServer-{preversion}-py3-none-any.whl', f'https://erri4.github.io/package/filesforpip/WebsocketServer-{version}-py3-none-any.whl')
     with open('README.md', 'w') as readme:
@@ -135,3 +135,11 @@ elif i == 'DBConnectionPool':
     current_directory = os.getcwd()
     pip_bat_path = os.path.join(current_directory, 'pip.bat')
     subprocess.run([pip_bat_path, 'DBConnectionPool', version, preversion])
+    with open('C:/users/reef/documents/websocket/requirements.txt', 'r') as requirements:
+        new = requirements.read().replace(f'https://erri4.github.io/package/filesforpip/DBConnectionPool-{preversion}-py3-none-any.whl', f'https://erri4.github.io/package/filesforpip/DBConnectionPool-{version}-py3-none-any.whl')
+    with open('C:/users/reef/documents/websocket/requirements.txt', 'w') as requirements:
+        requirements.write(new)
+    with open('README.md', 'r') as readme:
+        new = readme.read().replace(f'https://erri4.github.io/package/filesforpip/DBConnectionPool-{preversion}-py3-none-any.whl', f'https://erri4.github.io/package/filesforpip/DBConnectionPool-{version}-py3-none-any.whl')
+    with open('README.md', 'w') as readme:
+        readme.write(new)
